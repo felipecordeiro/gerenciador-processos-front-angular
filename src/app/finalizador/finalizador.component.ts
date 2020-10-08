c755dcc037e2e0148510cad318c2e9da919c8edc
Felipe Cordeiro Ramos <felipecordeiro4@gmail.com>
2020-10-08 01:11:29 -0300

    salvando parecer do processo e listando na tabela

--- src/app/finalizador/finalizador.component.ts
+++ src/app/finalizador/finalizador.component.ts
@@ -2,6 +2,7 @@ import { ProcessoService } from './../../providers/processo.service';
 import { Processo } from './../../to/processo';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-finalizador',
@@ -10,10 +11,13 @@ import { Router } from '@angular/router';
 })
 export class FinalizadorComponent implements OnInit {
 
-  view: 'incluir' | 'visualizar' | 'atribuicao'
+  view: 'incluir' | 'visualizar'
   processos: Processo[] = []
+  processoSave: Processo = new Processo
+  form: FormGroup
 
   constructor(private router: Router,
+    private fb: FormBuilder,
     private processoService: ProcessoService) { }
 
   ngOnInit(): void {
@@ -21,10 +25,42 @@ export class FinalizadorComponent implements OnInit {
     this.carregarProcessos()
   }
 
+  iniciarForm() {
+    this.form = this.fb.group({
+      descricaoParecer: [null, Validators.required]
+    })
+  }
+
   carregarProcessos() {
     this.processoService.getAll().subscribe(res => {
       this.processos = res.filter(obj => obj.pendenteParecer)
     })
+  }
+
+  incluirParecer(processo: Processo){
+    this.processoSave = processo
+    this.iniciarForm()
+    this.view = 'incluir'
+  }
+
+  botaoSalvarDescricaoParecer(){
+    this.processoSave.descricaoParecer = this.form.get('descricaoParecer').value
+    this.processoSave.pendenteParecer = false
+    this.processoService.update(this.processoSave).subscribe(res => {
+
+      this.visualizacao()
+    }, error => {
+
+    })
+  }
+
+  voltar(){
+    this.visualizacao()
+  }
+
+  visualizacao() {
+    this.view = 'visualizar'
+    this.carregarProcessos()
   }
 
   logout() {
